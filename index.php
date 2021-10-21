dbffbfa979b1e5c0ed9d7a4956281e0746c18bca
giov <pintogiovanni818@gmail.com>
2021-10-21 16:50:30 +0200

    setting and getting

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ class Movie
 {
 #instance variables
     public $title;
-    public $description;
     public $rating;
+    public $description;
+
 #constructor function
     public function __construct($_title, $_rating, $_description)
     {
@@ -14,16 +15,39 @@ class Movie
         $this->description = $_description;
         #rederclared instance variables
     }
-#add attributes Method
-public function addAttributes($title,$description,$rating) {
+# setting and getting
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-    echo 'ciao0';
+    public function getRating()
+    {
+        return $this->rating;
+    }
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+    }
+
+    public function getDescription()
+    {
+        return $this->rating;
+    }
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
 
 }
+$movies = new Movie($_GET['title'], $_GET['rating'], $_GET['description']);
 
-}
-$movie->addAttributes();
-$movies =  new Movie($_title, $_rating, $_description);
 var_dump($movies);
-
-
+$movies->setTitle('string1');
+$movies->setRating('string2');
+$movies->setDescription('string3');
+var_dump($movies);
